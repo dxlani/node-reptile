64d66432e94b19a21fdc2fbaa6847012e541857a
dingxiaolin <dxlani001@163.com>
2018-10-15 20:41:51 +0800

    init node 爬虫

---
+++ test.js
@@ -0,0 +1,14 @@
+var sleep = function (time) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(console.log('999'));
+        }, time);
+    })
+};
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var start = function(){
+    arr.forEach(async ()=>{
+        await sleep(2000)
+    })
+}
+start();
